7c736ba3339bc797a56a362564bd067d7fd1bccc
TSFSeanSimon <TSFSeanSimon@users.noreply.github.com>
2018-05-15 21:34:12 +1000

    + Intro Logo

--- Assets/Scripts/MenuScripts/MainLoader.cs
+++ Assets/Scripts/MenuScripts/MainLoader.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class MainLoader : MonoBehaviour {
 
     public string levelToLoad;
+    public float delayTime;
 
 	void Start () {
         PlayerPrefs.DeleteKey("SkipTutorial");
         GetComponent<ResetGame>().Reset();
-        Application.LoadLevel(levelToLoad);
+        StartCoroutine(NextLevel(delayTime));
+
 	}
+
+    IEnumerator NextLevel (float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Application.LoadLevel(levelToLoad);
+
+    }
 }
